e74e694fa9f42ed037416a826fdc8df869695742
tuckerTU <tuckerishere@gmail.com>
2021-02-02 09:07:57 -0700

    commit tree c027c471c75ab363b8ac6554d5a91c7010ab77b6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    attrs = params
+    @article = Article.create(attrs)
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    attrs = params[:article]
+    @article.update(attrs)
+    redirect to "/articles/#{@article.id}"
+  end
+
+    delete '/articles/:id' do
+    id = params[:id]
+    Article.destroy(id)
+    redirect to '/articles'
+    end
+
 end
